e0f6ee179292cfb2bdc78b2fb7304ffccbf9c90a
MatchaFierce <cesarvaldez.work@gmail.com>
2018-11-03 12:07:11 -0600

    Fixed errors

--- src/state/reducers/DependentsReducer.js
+++ src/state/reducers/DependentsReducer.js
@@ -1,4 +1,4 @@
-import { GET_DEPENDANTS_SUCCESS, GET_DEPENDANTS_ERROR, LOADING_DEPENDANTS } from './../actions/UserActions';
+import { GET_DEPENDENTS_SUCCESS, GET_DEPENDENTS_ERROR, LOADING_DEPENDENTS } from './../actions/DependentsActions';
 
 
 /**
@@ -19,7 +19,7 @@ const initialState = {
  */
 const DependantsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_DEPENDANTS_SUCCESS:
+    case GET_DEPENDENTS_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -27,35 +27,40 @@ const DependantsReducer = (state = initialState, action) => {
       }
 
 
-    case GET_DEPENDANTS_ERROR:
+    case GET_DEPENDENTS_ERROR:
       return {
         ...state,
         loading: false,
         errorMessage: action.error
       }
 
+      case LOADING_DEPENDENTS:
+        return {
+          ...state,
+          loading: true
+        }
 
-    case GET_DEPENDENT:
-      break;
+    // case GET_DEPENDENT:
+    //   break;
+    //
+    // case POST_DEPENDENT:
+    //   return state.concat([action.data]);
+    //
+    // case UPDATE_DEPENDANT:
+    //   return state.map((dependant) => {
+    //     if (dependant.id === action.id) {
+    //       return {
+    //         ...dependant,
+    //         name: action.data.newName,
+    //         fatherName: action.data.newFatherName,
+    //         motherName: action.data.newMotherName,
+    //       }
+    //     } else return dependant;
+    //   })
 
-    case POST_DEPENDENT:
-      return state.concat([action.data]);
 
-    case UPDATE_DEPENDANT:
-      return state.map((dependant) => {
-        if (dependant.id === action.id) {
-          return {
-            ...dependant,
-            name: action.data.newName,
-            fatherName: action.data.newFatherName,
-            motherName: action.data.newMotherName,
-          }
-        } else return dependant;
-      })
-
-
-    case DELETE_DEPENDANT:
-      return state.filter((dependant) => dependant.id !== action.id)
+    // case DELETE_DEPENDANT:
+    //   return state.filter((dependant) => dependant.id !== action.id)
 
 
     default:
